fa524cf00b7ea0b12a95c737768e1e73289b8f67
Emily Jiang <emily.s.jiang@gmail.com>
2022-10-01 16:14:00 -0400

    basic insult setup

--- src/pages/Newtab/Newtab.jsx
+++ src/pages/Newtab/Newtab.jsx
@@ -7,19 +7,10 @@ const Newtab = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Edit <code>src/pages/Newtab/Newtab.js</code> and save to reload.
+          insert insult here
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-        <h6>The color of this paragraph is defined using SASS.</h6>
       </header>
     </div>
   );
