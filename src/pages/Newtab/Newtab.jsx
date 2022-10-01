4ee9a6d557e4d99afc2cb5d0919606353d94ac23
Emily Jiang <emily.s.jiang@gmail.com>
2022-10-01 17:31:29 -0400

    basic insult construction

--- src/pages/Newtab/Newtab.jsx
+++ src/pages/Newtab/Newtab.jsx
@@ -9,12 +9,32 @@ const backgroundColors = ['#BD9EA8',
                           '#3C6E71',
                           '#6796A2',];
 
+const insultTemplates = ['you are such a {0} {1}'];
+
+const insultNouns = ['doorknob'];
+
+const insultAdjectives = ['antiquated','asinine','banal','brazen','catty','churlish','clammy','contrary','daft','damned','deceitful','decrepit','deficient','degrading','deleterious','devoid','dim','dismal','disreputable','dopey','dreary','drunken','dubious','dysfunctional','fatuous','feckless','glib','grotesque','imbecilic','impertinent','indecorous','indiscreet','infantile','jejune','lurid','malevolent','misshapen','morbid','moribund','mundane','petulant','puerile','rambunctious','repugnant','truculent','unkempt','vainglorious','vapid'];
+
+if (!String.prototype.format) {
+  String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) { 
+      return typeof args[number] != 'undefined'
+        ? args[number]
+        : match
+      ;
+    });
+  };
+}
+
+const insult = insultTemplates[Math.floor(Math.random()*insultTemplates.length)].format(insultAdjectives[Math.floor(Math.random()*insultAdjectives.length)], insultNouns[Math.floor(Math.random()*insultNouns.length)])
+
 const Newtab = () => {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          insert insult here
+          {insult}
         </p>
       </header>
     </div>
